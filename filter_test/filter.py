baa032bd20da6c38720042dbecad9299a0f71b7d
ChangYu Huang <jack.mainwork@gmail.com>
2023-12-18 14:32:28 +0800

    Change the ToS and Duration to collect more data to analyze.

--- filter_test/filter.py
+++ filter_test/filter.py
@@ -1,4 +1,6 @@
 import pandas as pd, csv
+from datetime import datetime
+from matplotlib import pyplot as plt
 
 
 def std_filter():
@@ -44,12 +46,51 @@ def MA_filter(n):
             writer.writerow([sum[0]/min(n,i+1),sum[1]/min(n,i+1)])
             print([sum[0]/min(n,i+1),sum[1]/min(n,i+1)])
             
+def cal_std(n):
+    '''
+    計算從i往前n筆的標準差
+    '''
+    raw_data = pd.read_csv('raw01.csv')
+    with open('std.csv','a',newline = '') as output:
+        writer = csv.writer(output,delimiter=',',lineterminator='\n')
+        i = n
+        while i < raw_data.size / 10:
+            group = raw_data[i-n:i]
+            std = group.std().iloc[9]
+            data = group.iloc[n-1].tolist()
+            data.append(std)
+            writer.writerow(data)
+            i += 1
 
 
+def draw_std(n,thred,exclude):
+    '''
+    計算從i往前n筆的標準差並繪圖，如果標準差大於s的話使用不同顏色標註
+    '''
+
+    raw_data = pd.read_csv('raw01.csv')
+    df = pd.DataFrame({"time":[],"weight":[],"std":[]})
+    df.columns = ["time","weight","std"]
+
+    i = n
+    count = 0
+    while i < raw_data.size / 10:
+        t = datetime(raw_data.iat[i,0],raw_data.iat[i,1],raw_data.iat[i,2],raw_data.iat[i,3],raw_data.iat[i,4],raw_data.iat[i,5],raw_data.iat[i,6])
+        if exclude and raw_data.iloc[i-n:i].std().iloc[9] > thred:
+            count += 1
+            pass
+        else:
+            df.loc[len(df)] = [pd.to_datetime(t),raw_data.iloc[i,9],int(raw_data.iloc[i-n:i].std().iloc[9] >= thred)]
+        i += 1
+    print("Remove " + str(count) + " records.")
+    df.plot.scatter(x = 'time', y = 'weight', c = 'std', cmap = 'viridis', s = 10)
+    plt.show()
 
 # std_filter()
 # MA_filter(800)
 # MA_filter(1000)
 # MA_filter(1200)
 # MA_filter(1600)
-MA_filter(40)
+# MA_filter(40)
+# cal_std(40)
+draw_std(40,1,False)
